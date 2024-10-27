19daf368898345dc9d82f1377ddceb797a2076a7
Harshavardhan Sasikumar <86889928+Harshavardhan-504@users.noreply.github.com>
2024-10-27 09:15:23 -0500

    Update app.py
    Traditional IRA

--- app.py
+++ app.py
@@ -205,3 +205,20 @@ def get_irs_roth_calculator():
                               sandbox_data["annual_tax_rate"],
                               sandbox_data["max_out"])
     return jsonify(result)   
+
+@app.route(/api/ira_trad_calculator/', methods=['POST'])
+def get_irs_trad_calculator():
+    sandbox_data=request.json
+
+    result=trad_ira_calculator(sandbox_data["current_age"],
+                              sandbox_data["retirement_age"],
+                              sandbox_data["current_balance"],
+                              sandbox_data["annual_contribution_percentage"],
+                              sandbox_data["expected_annual_return"],
+                              sandbox_data["annual_salary_growth_rate"],
+                              sandbox_data["annual_tax_rate"],
+                              sandbox_data["max_out"])
+    
+    return jsonify(result)
+
+
