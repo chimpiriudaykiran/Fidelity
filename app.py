e8551b3fc510f41c4e9b60a768aa5f6972a16788
Lakshmi Sairam Kakarla <67178651+sairam-kakarla@users.noreply.github.com>
2024-10-27 03:45:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -132,32 +132,28 @@ def update_userinfo(user_id):
         db.users_info.insert_one(data)
         return jsonify({"message": "User information created successfully"}), 201
 
-@app.route('/api/trs_calculator/', methods=['GET'])
-def get_trs_calculator():
-    # FETCH data from db, if doesn't exists, redirect to data creation page
-    sandbox_data=None
-    return jsonify(trs_retirement_calculator(sandbox_data["current_age"],
-                              sandbox_data["current_years_service"],
-                              sandbox_data["retirement_age"],
-                              sandbox_data["current_salary"],
-                              sandbox_data["annual_growth_rate"],
-                              sandbox_data["penalty_rate_per_year"]))
+@app.route('/dashboard')
+def index():
+    # TODO : Get user data about the TRS montly pension
+    # TODO: Salary increment data over years.
+    # Final Roth IRS Balance
+    # Final Roth Tranditional Balance
+    # Final 403(b) Balance
 
 
-@app.route('/api/trs_calculator/', methods=['POST'])
-def post_trs_calculator():
+
+@app.route('/api/403b_calculator/', methods=['POST'])
+def post_403b_calculator():
     # TODO: Look for data if exists, update, else create
-    return redirect(url_for("/api/trs_calculator/"))
-
-
-
-
-@app.route('/api/403b_calculator/', methods=['GET'])
-def get_403b_calculator():
-    sandbox_data=None
-    return jsonify(trs_retirement_calculator(sandbox_data["current_age"],
+    sandbox_data=request.json
+    result=retirement_403b_calculator(sandbox_data["current_age"],
                               sandbox_data["retirement_age"],
-                              sandbox_data["current_403b_balance"],
+                              sandbox_data["current_balance"],
                               sandbox_data["current_salary"],
-                              sandbox_data["annual_growth_rate"],
-                              sandbox_data["penalty_rate_per_year"]))
+                              sandbox_data["annual_contribution_percentage"],
+                              sandbox_data["employer_match_percentage"],
+                              sandbox_data["expected_annual_return"],
+                              sandbox_data["annual_salary_growth_rate"],
+                              sandbox_data["annual_investment_fee_rate"],
+                              sandbox_data["max_out"])
+    return jsonify(result)
